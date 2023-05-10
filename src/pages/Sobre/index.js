1cd44398bca4e001dfe8c4bbdfb9c731690893a8
George Lucas Apps <44735373+georgelucasdev@users.noreply.github.com>
2023-05-10 11:06:17 -0300

    modified:   src/global.module.scss 	new file:   src/img/cisco-python1.png 	new file:   src/img/cisco-python2.png 	deleted:    src/img/python-essentials-1.1.png 	modified:   src/pages/Sobre/index.js

--- src/pages/Sobre/index.js
+++ src/pages/Sobre/index.js
@@ -1,32 +1,32 @@
 import styles from '../../global.module.scss'
-import { FaInstagram, FaLinkedin, FaFacebook, FaTwitter, FaReact, FaNode, } from 'react-icons/fa';
+import { FaReact, FaNode, } from 'react-icons/fa';
 import { SiNextdotjs, SiPython, SiFlutter, SiJavascript } from 'react-icons/si';
-import python from '../../img/python-essentials-1.1.png'
+import ciscopython1 from '../../img/cisco-python1.png';
+import ciscopython2 from '../../img/cisco-python2.png';
 import React from 'react';
 
 export default function Sobre() {
   return (
     <>
-        <main className={styles.container} >
-          <div className={styles.containerHeader} > 
-            <section>
-              <br/>
-              <br/>
-              <h1><strong><center>George Lucas</center></strong></h1>
-              <br/>
-              <br/>
-              <p>
-                <center>
+      <main className={styles.container} >
+        <div className={styles.containerHeader} >
+          <section>
+            <br />
+            <h1><strong><center>George Lucas</center></strong></h1>
+            <br />
+            <br />
+            <p>
+              <center>
                 Em constante evolução em desenvolvimento web e mobile; focado em criar novas experiências
                 para o usuário com as tecnologias e frameworks mais utilizadas do mercado :
                 ReactJS, JavaScript, Flutter, ReactNative, Python .
                 De caráter honesto, busca o estímulo, organização, compartilhamento de conhecimento e o
-                  contínuo desenvolvimento pessoal e profissional do grupo de trabalho.
-                </center>
-              </p>
-              <br />
-              <br />
-              <div className={styles.container4}>
+                contínuo desenvolvimento pessoal e profissional do grupo de trabalho.
+              </center>
+            </p>
+            <br />
+            <br />
+            <div className={styles.container4}>
               <section className={styles.sectionItens}>
                 <a><FaNode size={40} /></a>
               </section>
@@ -45,26 +45,28 @@ export default function Sobre() {
               <section className={styles.sectionItens}>
                 <a><SiJavascript size={40} /></a>
               </section>
+            </div>
+            <br />
+            <br />
+            <h3><strong><center>
+              Certificações
+            </center></strong></h3>
+            <div className={styles.bb}>
+              <div className={styles.certificacao}>
+                <a href="https://www.credly.com/badges/4346d242-d25a-4490-ac73-571979364c08/public_url" target="_blank" rel="noreferrer">
+                  <img src={ciscopython1} />
+                </a>
+                <a href="https://www.credly.com/badges/fc0cd9a5-b62d-4b33-84b3-9337307b4da9/public_url" target="_blank" rel="noreferrer">
+                  <img src={ciscopython2} />
+                </a>
               </div>
-              <br />
-              <br/>
-              <br/>
-              <br/>
-              <div className={styles.container}>
-              <h3><strong><center>
-                Certificações
-                </center></strong></h3>
-                <br />
-              <section className={styles.sectionItens}>
-              <center><a href="https://www.credly.com/badges/4346d242-d25a-4490-ac73-571979364c08/public_url" target="_blank" rel="noreferrer">
-                <img src={python} size={40} />
-                </a></center>
-              </section>
-              </div>
-            </section>
-          </div>
-        </main>
-
+            </div>
+          </section>
+        </div>
+        <br />
+        <br />
+        <br />
+      </main>
     </>
   );
 }
