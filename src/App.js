3d2a744af8ee9ebd01940e8efd45e9391cff886c
George Lucas Apps <44735373+georgelucasdev@users.noreply.github.com>
2024-06-03 15:57:19 -0300

    modified:   src/App.js 	modified:   src/components/Popup.css 	modified:   src/global.module.scss 	modified:   src/pages/Home/index.js

--- src/App.js
+++ src/App.js
@@ -5,22 +5,11 @@ import RoutesApp from './routes';
 import Popup from './components/Popup';
 
 function App() {
-  const [showPopup, setShowPopup] = useState(true);
-  const navigate = useNavigate();
 
-  const handleClose = () => {
-    setShowPopup(false);
-  };
-
-  const handleButtonClick = (path) => {
-    navigate(path);
-    setShowPopup(false);  // Certifique-se de que o popup feche após a navegação
-  };
 
   return (
     <>
       <RoutesApp />
-      {showPopup && <Popup show={showPopup} onClose={handleClose} onButtonClick={handleButtonClick} />}
     </>
   );
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -6,10 +6,13 @@ import { FaReact, FaNode, } from 'react-icons/fa';
 import { SiNextdotjs, SiPython, SiFlutter, SiJavascript } from 'react-icons/si';
 import ciscopython1 from '../../img/cisco-python1.png';
 import ciscopython2 from '../../img/cisco-python2.png';
+import Popup from '../../components/Popup';
+import { useNavigate } from 'react-router-dom';
 
 export default function Home() {
   const [showInfo1, setShowInfo1] = useState(false);
   const [showInfo2, setShowInfo2] = useState(false);
+  
 
   const handleButton1Click = () => {
     setShowInfo1(!showInfo1);
@@ -20,8 +23,21 @@ export default function Home() {
     setShowInfo2(!showInfo2);
     setShowInfo1(false);
   };
+
+  const [showPopup, setShowPopup] = useState(true);
+  const navigate = useNavigate();
+
+  const handleClose = () => {
+    setShowPopup(false);
+  };
+
+  const handleButtonClick = (path) => {
+    navigate(path);
+    setShowPopup(false);  // Certifique-se de que o popup feche após a navegação
+  };
 return(
   <>
+  {showPopup && <Popup show={showPopup} onClose={handleClose} onButtonClick={handleButtonClick} />}
   <br/>
   <container className={styles.container}> 
         <section>
@@ -53,60 +69,45 @@ return(
     <br/>
     <hr className={styles.hr3}/>
     <br/>
-  <container className={styles.container1}> 
-            <section>
-              <section className={styles.container}>
-                        <div className={styles.container4}>
-                            <section className={styles.sectionItens}>
-                              <a><FaNode size={40} /></a>
-                            </section>
-                            <section className={styles.sectionItens}>
-                              <a><FaReact size={40} /></a>
-                            </section>
-                            <section className={styles.sectionItens}>
-                              <a><SiNextdotjs size={40} /></a>
-                            </section>
-                            <section className={styles.sectionItens}>
-                              <a><SiPython size={40} /></a>
-                            </section>
-                            <section className={styles.sectionItens}>
-                              <a><SiFlutter size={40} /></a>
-                            </section>
-                            <section className={styles.sectionItens}>
-                              <a><SiJavascript size={40} /></a>
-                            </section>
-                        </div>
+          <container className={styles.container1}> 
+                    <section>
+                      <section className={styles.container}>
+                                <div className={styles.container4}>
+                                    <section className={styles.sectionItens}>
+                                      <a><FaNode size={40} /></a>
+                                    </section>
+                                    <section className={styles.sectionItens}>
+                                      <a><FaReact size={40} /></a>
+                                    </section>
+                                    <section className={styles.sectionItens}>
+                                      <a><SiNextdotjs size={40} /></a>
+                                    </section>
+                                    <section className={styles.sectionItens}>
+                                      <a><SiPython size={40} /></a>
+                                    </section>
+                                    <section className={styles.sectionItens}>
+                                      <a><SiFlutter size={40} /></a>
+                                    </section>
+                                    <section className={styles.sectionItens}>
+                                      <a><SiJavascript size={40} /></a>
+                                    </section>
+                                </div>
+                        </section>
+                          <br />
+                          <br />
+                            <div className={styles.bb}>
+                              <div className={styles.certificacao}>
+                                <a href="https://www.credly.com/badges/4346d242-d25a-4490-ac73-571979364c08/public_url" target="_blank" rel="noreferrer">
+                                  <img src={ciscopython1} />
+                                </a>
+                                <a href="https://www.credly.com/badges/fc0cd9a5-b62d-4b33-84b3-9337307b4da9/public_url" target="_blank" rel="noreferrer">
+                                  <img src={ciscopython2} />
+                                </a>
+                              </div>
+                            </div>
                 </section>
-                  <br />
-                  <br />
-                    <div className={styles.bb}>
-                      <div className={styles.certificacao}>
-                        <a href="https://www.credly.com/badges/4346d242-d25a-4490-ac73-571979364c08/public_url" target="_blank" rel="noreferrer">
-                          <img src={ciscopython1} />
-                        </a>
-                        <a href="https://www.credly.com/badges/fc0cd9a5-b62d-4b33-84b3-9337307b4da9/public_url" target="_blank" rel="noreferrer">
-                          <img src={ciscopython2} />
-                        </a>
-                      </div>
-                    </div>
-        </section>
-            <br/>
-
-            <section>
-                <div style={{ display: 'flex', justifyContent: 'center' , marginLeft: '3rem' , marginRight: '3rem' , height: '580px'}}>
-                    <div style={{marginLeft: '3rem' , marginRight: '3rem'}}>
-                      <iframe
-                        title="Postagem do Instagram"
-                        srcDoc='<blockquote class="instagram-media" data-instgrm-permalink="https://www.instagram.com/georgelucasnovo/" data-instgrm-version="13"></blockquote> <script async src="//www.instagram.com/embed.js"></script>'
-                        width="340"
-                        height="450"
-                        allowtransparency="true"
-                      >
-                      </iframe>
-                    </div>
-                  </div>
-              </section>
-  </container>
+                    <br/>
+          </container>
   </>
  );
 }
